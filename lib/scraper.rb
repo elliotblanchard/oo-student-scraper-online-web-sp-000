c11b1657832950b4a9f95d50493e2b90858437af
Elliot Blanchard <public@elliotblanchard.com>
2020-04-28 00:57:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,33 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     full_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/" + profile_url
     doc = Nokogiri::HTML(open(full_url))
+
+    profile = {}
+    
+    twitterURL = nil;
+    linkedinURL = nil;
+    githubURL = nil;
+    blogURL = nil;
+
+    #Get the social URLs (if they exist)
+    for doc.css("div.social-icon-container a").each |item| do
+      if item.attribute("href").value.include? "twitter"
+        profile[:twitter] = item.attribute("href").value
+      elsif item.attribute("href").value.include? "linkedin"
+        profile[:linkedin] = item.attribute("href").value
+      elsif item.attribute("href").value.include? "github"
+        profile[:github] = item.attribute("href").value
+      else
+        profile[:blog] = item.attribute("href").value  
+      end    
+    end
+
+    profile[:profile_quote] = doc.css("div.profile-quote").text
+    profile[:bio] = doc.css("div.description-holder p").text
+    
     binding.pry
+    
+    #my_hash[:key] = "value"
     #Use Joe Burgess - he has every kind of link: https://learn-co-curriculum.github.io/student-scraper-test-page/students/joe-burgess.html
     #Social icon container:
         #<div class="social-icon-container"> / doc.css("div.social-icon-container")
@@ -45,7 +71,7 @@ class Scraper
     #:linkedin <a href="https://www.linkedin.com/in/james-novak-3ba89266"><img class="social-icon" src="../assets/img/linkedin-icon.png"></a>
     #:github <a href="http://www.github.com/jamesnvk"><img class="social-icon" src="../assets/img/github-icon.png"></a>
     #:blog <a href="http://joemburgess.com/"><img class="social-icon" src="../assets/img/rss-icon.png"></a> - tricky
-    #all social links are contained in the array:
+    #social links are contained in the array:
     #...doc.css("div.social-icon-container a")[0].attribute("href").value
     #...but different people have different ones, so you'll need to search for the strings "twitter" "linkedin" "github" "instagram"
     #...the blog can be anything, so it would just have to be the URL that ISN'T one of those
